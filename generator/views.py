0c0793ad8dea9f365813ae7e710e3736130aa79e
ragilhadi <ragilprasetyo310@gmail.com>
2020-06-12 01:15:39 +0700

    Create Generator Program

--- generator/views.py
+++ generator/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+import random
+import string
 # Create your views here.
 
 
@@ -8,4 +9,26 @@ def home(request):
 
 
 def generate(request):
-    return render(request, 'generator/generate.html')
+    passgenerate = ''
+    # list lowercase
+    lowerCase = string.ascii_lowercase
+    charachter = list(lowerCase)
+    # list uppercase
+    if request.GET.get('uppercase'):
+        upperCase = string.ascii_uppercase
+        charachter.extend(list(upperCase))
+
+    if request.GET.get('number'):
+        number = list('1234567890')
+        charachter.extend(list(number))
+
+    if request.GET.get('special'):
+        special = list('!&*#?+~')
+        charachter.extend(list(special))
+
+    length = int(request.GET.get('length'))
+
+    for x in range(length):
+        passgenerate += random.choice(charachter)
+
+    return render(request, 'generator/generate.html', {'password': passgenerate})
